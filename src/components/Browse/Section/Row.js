c6f390d285e7f32c65086623d71e341cfba0beff
Koras02 <wjdgh0727@gmail.com>
2021-09-11 03:39:28 +0900

    아이템 배열 정리

--- src/components/Browse/Section/Row.js
+++ src/components/Browse/Section/Row.js
@@ -1,2 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
+function Row (props) {
+    const {title} = props;
+
+    const [movies,setMovies] = useState([]);
+    const [movieDetail, setMovieDetail] = useState([]);
+    const [ modalOpen, setModalOpen ] = useState(false);
+    const [ cardOpen, setCardOpen ] = useState(false);
+
+
+    const openModal = () => {
+        setModalOpen(true)
+        setCardOpen(false)
+    }
+
+    const closeModal = () => {
+        setModalOpen(false)
+    } 
+    const openCard = (e, movie) => {
+        setCardOpen(true)
+        setMovieDetail(movie)
+    }
+
+    function getMovie() {
+        axios({
+            method:"GET",
+        })
+    }
+
+    return (
+       <div>
+           {title}
+       </div>
+    )
+}
+
+export default Row;
--- src/components/Browse/index.js
+++ src/components/Browse/index.js
@@ -1,23 +1,56 @@
 import axios from "axios";
-import React from "react";
+import React, { Profiler } from "react";
 import { useEffect, useState} from "react";
 import requests from "../../utils/requests";
+import Fuse from "fuse.js";
+import { Card } from "react-bootstrap";
  
-import Row from './Section/Row'
-
-export default function Browse({ children, ...restProps }) {
  
+export default function Browse({ children,slides, ...restProps }) {
+    const [movies, seMovies] = useState([]);
+    const [slidesRows, setSlideRows] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [profile, setProfile] = useState({});
+ 
+    
+    useEffect(() => {
+        const fuse = new Fuse(slidesRows, {keys: ['data.description', 'data.title', 'data.genre']});
+        const results = fuse.search(searchTerm).map(({ item }) => item);
 
-    return (
+        if (slidesRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+             setSlideRows(results);
+
+        } else {
+            setSlideRows(slides);
+        }
+    }, [searchTerm]);
+
+
+    return profile.dispalyName ? (
         <>
-        <div className="movies" title="트렌딩">
-           {/* <Row title="지금 뜨는 콘텐츠"   /> */}
-        </div>
-        </>
-    )
+      <Card.Group>
+        {slidesRows.map((slideItem) => (
+          <Card >
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={item.docId} item={item}>
+                  <Card  />
+                  <Card.Meta>
+                    <Card.SubTitle>{item.title}</Card.SubTitle>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.Meta>
+                </Card.Item>
+                ))}
+                </Card.Entities>
+                </Card>
+         ))}
+         </Card.Group>
+         </>
+     ) : (
+             <div></div>
+        )
 }
 
 
-
-// Trending 영화
  
